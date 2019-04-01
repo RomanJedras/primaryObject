7cd6a7b0598ecd0c46b8ff5bfc1a380404750362
Roman Jędras <romanjedras@o2.pl>
2019-04-01 11:20:04 +0200

    Add new class Button & craete method -> button

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ onePlus.printInfo();
 SamsungGalaxyS6.printSystem();
 iPhone6S.printSystem();
 onePlus.printSystem();
+
+
+function Button(text) {
+    this.text = text || 'Hello';
+}
+
+Button.prototype = {
+    create: function() {
+        const self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function() {
+            alert(self.text);
+        });
+        document.body.appendChild(this.element);
+    }
+}
+
+const btn1 = new Button('Hello!');
+btn1.create();
